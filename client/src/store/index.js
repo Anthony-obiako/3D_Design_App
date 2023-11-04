fdab38121d3d835149b2e6df98ff5c89b9c8cde8
Anthony Obiako <anthonyobiakoc@gmail.com>
2023-11-04 12:20:49 +0100

    added buttons to navigate through home and custom page

---
+++ client/src/store/index.js
@@ -0,0 +1,12 @@
+import { proxy } from "valtio";
+
+const state = proxy({
+  intro: true,
+  color: "#EFBD48",
+  isLogoTexture: true,
+  isFullTexture: false,
+  logoDecal: "./threejs.org",
+  fullDecal: "./threejs.org",
+});
+
+export default state;
